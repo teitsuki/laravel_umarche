3845830b03732175bd3cb0a2f7cc48ecae41b6bb
teitsuki <teitsuki@icloud.com>
2021-08-17 21:35:22 +0900

    image index

--- resources/views/owner/images/index.blade.php
+++ resources/views/owner/images/index.blade.php
@@ -10,9 +10,12 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <x-flash-message status="session('status')" />
-                    @foreach ($shops as $shop)
+                    <div class="flex justify-end mb-4"> 
+                        <button onclick="location.href='{{ route('admin.owners.create')}}'" class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">新規登録する</button>                        
+                    </div>
+                    @foreach ($images as $image)
                         <div class="w-1/2 p-4">
-                            <a href="{{ route('owner.shops.edit', ['shop' => $shop->id]) }}">
+                            <a href="{{ route('owner.images.edit', ['image' => $image->id]) }}">
                                 <div class="border rounded-md p-4">
                                     <div class="mb-4">
                                         @if ($shop->is_selling)
@@ -21,12 +24,15 @@
                                         <span class="border p-2 rounded-md bg-red-400 text-white">停止中</span>
                                         @endif
                                     </div>
+
                                     <div class="text-xl">{{ $shop->name }}</div>
-                                    <x-thumbnail :filename="$shop->filename" type="shops" />
+                                    <x-thumbnail :filename="$shop->filename" type="products" />
+
                                 </div>
                             </a>
                         </div>
                     @endforeach
+                    {{ $images->links() }}
                 </div>
             </div>
         </div>
