c189d5af11d2d2c2cb097bee5314a4a1d2a6af17
teitsuki <teitsuki@icloud.com>
2021-08-17 10:21:18 +0900

    リソースコントローラ

--- routes/admin.php
+++ routes/admin.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Admin\Auth\NewPasswordController;
 use App\Http\Controllers\Admin\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Admin\Auth\RegisteredUserController;
 use App\Http\Controllers\Admin\Auth\VerifyEmailController;
+use App\Http\Controllers\Admin\OwnersController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -25,11 +26,12 @@ Route::get('/', function () {
     return view('admin.welcome');
 });
 
+Route::resource('owners', OwnersController::class)
+    ->middleware('auth:admin');
+
 Route::get('/dashboard', function () {
     return view('admin.dashboard');
 })->middleware(['auth:admin'])->name('dashboard');
-
-require __DIR__ . '/auth.php';
 
 Route::get('/register', [RegisteredUserController::class, 'create'])
     ->middleware('guest')
