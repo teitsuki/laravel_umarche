ac6f937d39d74ea04f2fc349cfce40cb07e66695
teitsuki <teitsuki@icloud.com>
2021-08-17 22:01:24 +0900

    image, create, validation

--- resources/views/owner/images/index.blade.php
+++ resources/views/owner/images/index.blade.php
@@ -11,7 +11,7 @@
                 <div class="p-6 bg-white border-b border-gray-200">
                     <x-flash-message status="session('status')" />
                     <div class="flex justify-end mb-4"> 
-                        <button onclick="location.href='{{ route('admin.owners.create')}}'" class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">新規登録する</button>                        
+                        <button onclick="location.href='{{ route('owner.images.create')}}'" class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">新規登録する</button>                        
                     </div>
                     @foreach ($images as $image)
                         <div class="w-1/2 p-4">
