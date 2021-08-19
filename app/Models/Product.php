35e8cc3493672460761ffa64fa240fe0a30686d5
teitsuki <teitsuki@icloud.com>
2021-08-19 14:17:25 +0900

    検索フォーム キーワード

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -127,4 +127,21 @@ class Product extends Model
             return;
         }
     }
+
+    public function scopeSearchKeyword($query, $keyword)
+    {
+        if (!is_null($keyword)) {
+            $spaceConvert = mb_convert_kana($keyword, 's');
+
+            $keywords = preg_split('/[\s]+/', $spaceConvert, -1, PREG_SPLIT_NO_EMPTY);
+
+            foreach ($keywords as $word) {
+                $query->where('products.name', 'like', '%' . $word . '%');
+            }
+
+            return $query;
+        } else {
+            return;
+        }
+    }
 }
