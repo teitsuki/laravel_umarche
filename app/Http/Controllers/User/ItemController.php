b89b9bde0794909b9c13cc5aaa81b56171219682
teitsuki <teitsuki@icloud.com>
2021-08-19 14:46:29 +0900

    テストメール

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -8,6 +8,8 @@ use App\Models\Product;
 use App\Models\Stock;
 use App\Models\PrimaryCategory;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\TestMail;
 
 
 class ItemController extends Controller
@@ -30,8 +32,11 @@ class ItemController extends Controller
             return $next($request);
         });
     }
+
     public function index(Request $request)
     {
+        Mail::to('test@example.com')->send(new TestMail());
+
         $categories = PrimaryCategory::with('secondary')->get();
 
         $products = Product::availableItems()
